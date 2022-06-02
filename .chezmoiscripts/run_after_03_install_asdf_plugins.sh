517ae466552f562055a3c95369d3f3749793e73d
Ryan Elliott-Potter <ryan@ryanep.com>
2022-06-02 09:25:30 +0100

    feat: update asdf scripts

---
+++ .chezmoiscripts/run_after_03_install_asdf_plugins.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+asdf_plugin_add() {
+    # Exit code 2 indicates the plugin is already installed so test and exit
+    # sucessfully if that is the error code.
+    asdf plugin add "$1" "${2:-}" || test $? = 2
+    asdf install $1
+}
+
+asdf_plugin_add nodejs
+asdf_plugin_add yarn
+asdf_plugin_add terraform
+asdf_plugin_add terragrunt
